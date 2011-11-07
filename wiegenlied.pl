4e2a8389f274cc7ac600716a2197c9846ece7975
Ondřej Garncarz <ondrej@garncarz.cz>
2011-11-07 01:07:07 +0000

    Přepis Fliesovy ukolébavky do Prologu.

---
+++ wiegenlied.pl
@@ -0,0 +1,232 @@
+notation((1, 1, g), (a, 1), 8).
+notation((1, 1, g), (f, 1), 4).
+notation((1, 2, g), (bes, 1), 8).
+notation((1, 3, g), (a, 1), 8).
+notation((1, 3, g), (f, 1), 8).
+notation((1, 4, g), (g, 1), 8).
+notation((1, 4, g), (e, 1), 8).
+notation((1, 5, g), (f, 1), 8).
+notation((1, 5, g), (d, 1), 8).
+notation((1, 6, g), (g, 1), 8).
+notation((1, 6, g), (e, 1), 8).
+notation((2, 1, g), (f, 1), 4).
+notation((2, 3, g), r, 8).
+notation((2, 4, g), r, 4).
+notation((2, 6, g), (f, 1), 8).
+notation((3, 1, g), (f, 1), 8).
+notation((3, 1, g), (d, 1), 8).
+notation((3, 2, g), (bes, 1), 8).
+notation((3, 2, g), (f, 1), 8).
+notation((3, 3, g), (bes, 1), 8).
+notation((3, 3, g), (f, 1), 8).
+notation((3, 4, g), (bes, 1), 8).
+notation((3, 4, g), (d, 1), 8).
+notation((3, 5, g), (c, 2), 8).
+notation((3, 5, g), (f, 1), 8).
+notation((3, 5, g), (d, 2), 8).
+notation((3, 6, g), (f, 1), 8).
+notation((4, 1, g), (c, 2), 4).
+notation((4, 1, g), (f, 1), 4).
+notation((4, 3, g), r, 8).
+notation((4, 4, g), r, 4).
+notation((4, 6, g), r, 8).
+
+notation((5, 1, g), (g, 1), 8).
+notation((5, 1, g), (e, 1), 8).
+notation((5, 2, g), (fis, 1), 8).
+notation((5, 2, g), (dis, 1), 8).
+notation((5, 3, g), (g, 1), 8).
+notation((5, 3, g), (e, 1), 8).
+notation((5, 4, g), (g, 1), 8).
+notation((5, 4, g), (e, 1), 8).
+notation((5, 5, g), (f, 1), 8).
+notation((5, 5, g), (d, 1), 8).
+notation((5, 6, g), (g, 1), 8).
+notation((5, 6, g), (e, 1), 8).
+notation((6, 1, g), (bes, 1), [4, 8, 4]).
+notation((6, 1, g), (g, 1), [8, 16, 8]).
+notation((6, 6, g), r, 8).
+notation((7, 1, g), (a, 1), 8).
+notation((7, 1, g), (f, 1), 8).
+notation((7, 2, g), (a, 1), 8).
+notation((7, 2, g), (f, 1), 8).
+notation((7, 3, g), (a, 1), 8).
+notation((7, 3, g), (f, 1), 8).
+notation((7, 4, g), (bes, 1), 8).
+notation((7, 4, g), (g, 1), 8).
+notation((7, 5, g), (a, 1), 8).
+notation((7, 5, g), (f, 1), 8).
+notation((7, 6, g), (bes, 1), 8).
+notation((7, 6, g), (g, 1), 8).
+notation((8, 1, g), (c, 2), [4, 8, 4]).
+notation((8, 1, g), (a, 1), [4, 8, 4]).
+notation((8, 6, g), r, 8).
+
+notation((9, 1, g), (d, 2), 8).
+notation((9, 1, g), (bes, 1), 8).
+notation((9, 1, g), (f, 1), 8).
+notation((9, 2, g), (d, 2), 8).
+notation((9, 2, g), (bes, 1), 8).
+notation((9, 2, g), (f, 1), 8).
+notation((9, 3, g), (d, 2), 8).
+notation((9, 3, g), (bes, 1), 8).
+notation((9, 3, g), (f, 1), 8).
+notation((9, 4, g), (d, 2), 8).
+notation((9, 4, g), (bes, 1), 8).
+notation((9, 4, g), (f, 1), 8).
+notation((9, 5, g), (cis, 2), 8).
+notation((9, 5, g), (bes, 1), 8).
+notation((9, 5, g), (f, 1), 8).
+notation((9, 6, g), (d, 2), 8).
+notation((9, 6, g), (bes, 1), 8).
+notation((9, 6, g), (f, 1), 8).
+notation((10, 1, g), (f, 2), [4, 8, 4]).
+notation((10, 1, g), (d, 2), [4, 8, 4]).
+notation((10, 1, g), (bes, 1), [4, 8, 4]).
+notation((10, 6, g), r, 8).
+notation((11, 1, g), (c, 2), 8).
+notation((11, 1, g), (a, 1), 8).
+notation((11, 1, g), (f, 1), 8).
+notation((11, 2, g), (c, 2), 8).
+notation((11, 2, g), (a, 1), 8).
+notation((11, 2, g), (f, 1), 8).
+notation((11, 3, g), (c, 2), 8).
+notation((11, 3, g), (a, 1), 8).
+notation((11, 3, g), (f, 1), 8).
+notation((11, 4, g), (c, 2), 8).
+notation((11, 4, g), (a, 1), 8).
+notation((11, 4, g), (f, 1), 8).
+notation((11, 5, g), (b, 1), 8).
+notation((11, 5, g), (gis, 1), 8).
+notation((11, 5, g), (f, 1), 8).
+notation((11, 6, g), (c, 2), 8).
+notation((11, 6, g), (a, 1), 8).
+notation((11, 6, g), (f, 1), 8).
+notation((12, 1, g), (f, 2), [4, 8, 4]).
+notation((12, 1, g), (c, 2), [4, 8, 4]).
+notation((12, 1, g), (a, 1), [4, 8, 4]).
+notation((12, 6, g), r, 8).
+
+notation((13, 1, g), (bes, 1), 8).
+notation((13, 1, g), (g, 1), 8).
+notation((13, 2, g), (c, 1), 8).
+notation((13, 2, g), (a, 1), 8).
+notation((13, 3, g), (bes, 1), 8).
+notation((13, 3, g), (g, 1), 8).
+notation((13, 4, g), (a, 1), 8).
+notation((13, 4, g), (f, 1), 8).
+notation((13, 5, g), (bes, 1), 8).
+notation((13, 5, g), (g, 1), 8).
+notation((13, 6, g), (a, 1), 8).
+notation((13, 6, g), (f, 1), 8).
+notation((14, 1, g), (g, 1), [4, 8, 4]).
+notation((14, 1, g), (e, 1), [4, 8, 4]).
+notation((14, 6, g), r, 8).
+notation((15, 1, g), (a, 1), 8).
+notation((15, 1, g), (f, 1), 8).
+notation((15, 2, g), (bes, 1), 8).
+notation((15, 2, g), (g, 1), 8).
+notation((15, 3, g), (a, 1), 8).
+notation((15, 3, g), (f, 1), 8).
+notation((15, 4, g), (g, 1), 8).
+notation((15, 4, g), (e, 1), 8).
+notation((15, 5, g), (a, 1), 8).
+notation((15, 5, g), (e, 1), 8).
+notation((15, 6, g), (g, 1), 8).
+notation((15, 6, g), (e, 1), 8).
+notation((16, 1, g), (f, 1), [4, 8, 4]).
+notation((16, 1, g), (c, 1), [4, 8, 4]).
+notation((16, 1, g), (a, 0), [4, 8, 4]).
+
+
+notation((1, 1, f), (f, 0), 4).
+notation((1, 1, f), (c, 1), 8).
+notation((1, 2, f), (d, 1), 8).
+notation((1, 3, f), (f, 0), 8).
+notation((1, 3, f), (c, 1), 8).
+notation((1, 4, f), (c, 0), 4).
+notation((1, 4, f), (bes, 0), 4).
+notation((1, 6, f), (c, 0), 8).
+notation((1, 6, f), (bes, 0), 8).
+notation((2, 1, f), (f, 0), 8).
+notation((2, 1, f), (a, 0), 8).
+notation((2, 2, f), (c, 1), 8).
+notation((2, 3, f), (c, 1), 8).
+notation((2, 4, f), (f, 0), 8).
+notation((2, 4, f), (a, 0), 8).
+notation((2, 5, f), (c, 1), 8).
+notation((2, 6, f), (c, 1), 8).
+notation((3, 1, f), (f, 0), 8).
+notation((3, 1, f), (bes, 0), 8).
+notation((3, 2, f), (d, 1), 8).
+notation((3, 3, f), (d, 1), 8).
+notation((3, 4, f), (f, 0), 8).
+notation((3, 4, f), (bes, 0), 8).
+notation((3, 5, f), (d, 1), 8).
+notation((3, 6, f), (d, 1), 8).
+notation((4, Beat, f), Pitch, Length) :- notation((2, Beat, f), Pitch, Length).
+
+notation((5, 1, f), (c, 0), 4).
+notation((5, 3, f), (c, 1), 8).
+notation((5, 4, f), (c, 0), 4).
+notation((5, 6, f), (c, 1), 8).
+notation((6, 1, f), (c, 0), 8).
+notation((6, 2, f), (e, 0), 8).
+notation((6, 3, f), (g, 0), 8).
+notation((6, 4, f), (c, 1), 8).
+notation((6, 5, f), (g, 0), 8).
+notation((6, 6, f), (c, 0), 8).
+notation((7, 1, f), (f, 0), 4).
+notation((7, 3, f), (c, 1), 8).
+notation((7, 4, f), (f, 0), 4).
+notation((7, 6, f), (c, 1), 8).
+notation((8, 1, f), (f, 0), 8).
+notation((8, 2, f), (a, 0), 8).
+notation((8, 3, f), (c, 1), 8).
+notation((8, 4, f), (f, 1), 8).
+notation((8, 5, f), (c, 1), 8).
+notation((8, 6, f), (f, 0), 8).
+
+notation((9, 1, f), (bes, -1), 8).
+notation((9, 2, f), (bes, 0), 8).
+notation((9, 3, f), (bes, 0), 8).
+notation((9, 4, f), (bes, -1), 8).
+notation((9, 5, f), (bes, 0), 8).
+notation((9, 6, f), (bes, 0), 8).
+notation((10, Beat, f), Pitch, Length) :- notation((9, Beat, f), Pitch, Length).
+notation((11, 1, f), (f, -1), 8).
+notation((11, 2, f), (f, 0), 8).
+notation((11, 3, f), (f, 0), 8).
+notation((11, 4, f), (f, -1), 8).
+notation((11, 5, f), (f, 0), 8).
+notation((11, 6, f), (f, 0), 8).
+notation((12, Beat, f), Pitch, Length) :- notation((11, Beat, f), Pitch, Length).
+
+notation((13, 1, f), (e, 0), 4).
+notation((13, 1, f), (c, 1), 4).
+notation((13, 3, f), (e, 0), 8).
+notation((13, 3, f), (c, 1), 8).
+notation((13, 4, f), (f, 0), 4).
+notation((13, 4, f), (c, 1), 4).
+notation((13, 6, f), (f, 0), 8).
+notation((13, 6, f), (c, 1), 8).
+notation((14, 1, f), (c, 0), 8).
+notation((14, 2, f), (e, 0), 8).
+notation((14, 3, f), (g, 0), 8).
+notation((14, 4, f), (c, 1), 8).
+notation((14, 5, f), (bes, 0), 8).
+notation((14, 6, f), (g, 0), 8).
+notation((15, 1, f), (f, 0), 4).
+notation((15, 1, f), (c, 1), 4).
+notation((15, 3, f), (f, 0), 8).
+notation((15, 3, f), (c, 1), 8).
+notation((15, 4, f), (c, 0), 4).
+notation((15, 4, f), (bes, 0), 4).
+notation((15, 6, f), (c, 0), 8).
+notation((15, 6, f), (bes, 0), 8).
+notation((16, 1, f), (f, 0), 8).
+notation((16, 2, f), (c, 0), 8).
+notation((16, 3, f), (a, -1), 8).
+notation((16, 4, f), (f, -1), 4).
+notation((16, 6, f), r, 8).
+
