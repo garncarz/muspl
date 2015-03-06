59635ced1607f772285ff98f31d0466a1b7c3622
Ondřej Garncarz <ondrej@garncarz.cz>
2015-03-06 03:41:19 +0100

    Another len seq extracted.

--- examples/wiegenlied.pl
+++ examples/wiegenlied.pl
@@ -4,7 +4,7 @@ extra composer('Bernhard Flies').
 extra scale{root:f, quality:major}.
 
 extra timeSignature(6, 8).
-% timeSignature(0, 6, 8). FIXME od jakého času platí
+% timeSignature(0, 6, 8). FIXME since when? till when?
 
 ♪ melody{start:(1, 1, g), relative:(a, 1, 8),
 	pitch:[0, 1, 0, -1, -2, -1, -2], len:[1*6, 2]}.
@@ -30,19 +30,20 @@ mel0r ♪= [0, r, r, 0, r, r].
 ♪ copyBars{from:2, to:4, cond:isStaff(f)}.
 
 
+'6long' ♪= [1*6, [4, 8, 4]].
 ♪ melody{start:(5, 1, g), relative:(g, 1, 8),
-	pitch:[0, -1:1, 0, 0, -1:1, 0, 2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0, -1:1, 0, 0, -1:1, 0, 2], len:'6long'}.
 ♪ melody{start:(5, 1, g), relative:(e, 1, 8),
-	pitch:[0, -1:1, 0, 0, -1:1, 0, 2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0, -1:1, 0, 0, -1:1, 0, 2], len:'6long'}.
 ♪ melody{start:(5, 1, f), relative:(c, 0, 4),
 	pitch:[0, 7, 0, 7], len:[1, 0.5, 1, 0.5]}.
 ♪ melody{start:(6, 1, f), relative:(c, 0, 8), pitch:[0, 2, 4, 7, 4, 0]}.
 
 '7mel' ♪= [0*3, 1, 0, 1, 2].
 ♪ melody{start:(7, 1, g), relative:(a, 1, 8), pitch:'7mel',
-	len:[1*6, [4, 8, 4]]}.
+	len:'6long'}.
 ♪ melody{start:(7, 1, g), relative:(f, 1, 8), pitch:'7mel',
-	len:[1*6, [4, 8, 4]]}.
+	len:'6long'}.
 ♪ melody{start:(7, 1, f), relative:(f, 0, 4),
 	pitch:[0, 4, 0, 4], len:[1, 0.5, 1, 0.5]}.
 
@@ -51,21 +52,21 @@ mel0r ♪= [0, r, r, 0, r, r].
 
 mel07 ♪= [0, 7, 7, 0, 7, 7].
 ♪ melody{start:(9, 1, g), relative:(d, 2, 8),
-	pitch:[0*4, -1:1, 0, 2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0*4, -1:1, 0, 2], len:'6long'}.
 ♪ melody{start:(9, 1, g), relative:(bes, 1, 8),
-	pitch:[0*6, 2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0*6, 2], len:'6long'}.
 ♪ melody{start:(9, 1, g), relative:(f, 1, 8),
-	pitch:[0*6, 3], len:[1*6, [4, 8, 4]]}.
+	pitch:[0*6, 3], len:'6long'}.
 ♪ melody{start:(9, 1, f), relative:(bes, -1, 8), pitch:mel07}.
 
 ♪ copyBars{from:9, to:10, cond:isStaff(f)}.
 
 ♪ melody{start:(11, 1, g), relative:(c, 2, 8),
-	pitch:[0*4, -1:1, 0, 3], len:[1*6, [4, 8, 4]]}.
+	pitch:[0*4, -1:1, 0, 3], len:'6long'}.
 ♪ melody{start:(11, 1, g), relative:(a, 1, 8),
-	pitch:[0*4, -1:1, 0, 2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0*4, -1:1, 0, 2], len:'6long'}.
 ♪ melody{start:(11, 1, g), relative:(f, 1, 8),
-	pitch:[0*6, 2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0*6, 2], len:'6long'}.
 ♪ melody{start:(11, 1, f), relative:(f, -1, 8), pitch:mel07}.
 
 ♪ copyBars{from:11, to:12, cond:isStaff(f)}.
@@ -73,9 +74,9 @@ mel07 ♪= [0, 7, 7, 0, 7, 7].
 
 '13mel' ♪= [0, 1, 0, -1, 0, -1, -2].
 ♪ melody{start:(13, 1, g), relative:(bes, 1, 8), pitch:'13mel',
-	len:[1*6, [4, 8, 4]]}.
+	len:'6long'}.
 ♪ melody{start:(13, 1, g), relative:(g, 1, 8), pitch:'13mel',
-	len:[1*6, [4, 8, 4]]}.
+	len:'6long'}.
 ♪ melody{start:(13, 1, f), relative:(c, 1, 4),
 	pitch:[0, 0, 0, 0], len:[1, 0.5, 1, 0.5]}.
 ♪ melody{start:(13, 1, f), relative:(e, 0, 4),
@@ -84,9 +85,9 @@ mel07 ♪= [0, 7, 7, 0, 7, 7].
 ♪ melody{start:(14, 1, f), relative:(c, 0, 8), pitch:[0, 2, 4, 7, 6, 4]}.
 
 ♪ melody{start:(15, 1, g), relative:(a, 1, 8),
-	pitch:[0, 1, 0, -1, 0, -1, -2], len:[1*6, [4, 8, 4]]}.
+	pitch:[0, 1, 0, -1, 0, -1, -2], len:'6long'}.
 ♪ melody{start:(15, 1, g), relative:(f, 1, 8),
-	pitch:[0, 1, 0, -1, -1, -1, -3], len:[1*6, [4, 8, 4]]}.
+	pitch:[0, 1, 0, -1, -1, -1, -3], len:'6long'}.
 ♪ melody{start:(16, 1, g), relative:(a, 0, [4, 8, 4]), pitch:[0]}.
 ♪ melody{start:(15, 1, f), relative:(c, 1, 4),
 	pitch:[0, 0, -1, -1], len:[1, 0.5, 1, 0.5]}.
