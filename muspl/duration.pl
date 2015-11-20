e881552aec47893ee27f49f9a52f3cc067ca53f2
Ondřej Garncarz <ondrej@garncarz.cz>
2015-11-20 03:32:40 +0100

    #1: duration.cmp(...).

--- muspl/duration.pl
+++ muspl/duration.pl
@@ -1,6 +1,6 @@
 :- module(duration, [
-	dursInvCmp/3
-	]).
+    dursInvCmp/3
+    ]).
 
 :- use_module(helpers).
 :- use_module(data).
@@ -8,72 +8,74 @@
 :- ['duration.plt'].
 
 Dur1.mul(Mul) := Dur2 :-
-	durationToBeats(Dur1.len, Beats1),
-	Beats2 is Beats1 * Mul,
-	beatsToDuration(Beats2, Len2),
-	Dur2 = Dur1.put(len, Len2).
+    durationToBeats(Dur1.len, Beats1),
+    Beats2 is Beats1 * Mul,
+    beatsToDuration(Beats2, Len2),
+    Dur2 = Dur1.put(len, Len2).
 
 Dur.beats() := Beats :-
-	Dur.get(negative) ->
-		durationToBeats(Dur.len, Beats2),
-		Beats is -Beats2;
-	durationToBeats(Dur.len, Beats).
+    Dur.get(negative) ->
+        durationToBeats(Dur.len, Beats2),
+        Beats is -Beats2;
+    durationToBeats(Dur.len, Beats).
 
 Dur1.add(Dur2) := Dur3 :-
-	sumDurationsInv(Dur1.len, SumInvDur1),
-	sumDurationsInv(Dur2.len, SumInvDur2),
-	DurSum is 1 / (SumInvDur1 + SumInvDur2),
-	normalizedDuration(DurSum, DurLen),
-	Dur3 = Dur1.put(len, DurLen).
+    sumDurationsInv(Dur1.len, SumInvDur1),
+    sumDurationsInv(Dur2.len, SumInvDur2),
+    DurSum is 1 / (SumInvDur1 + SumInvDur2),
+    normalizedDuration(DurSum, DurLen),
+    Dur3 = Dur1.put(len, DurLen).
 
 %% durationToBeats(+Duration, -Beats)
 % True if Duration(s) take Beats of beats.
 durationToBeats(0, 0) :- !.
 durationToBeats(Duration, Beats) :-
-	number(Duration), Duration > 0,
-	timeSignature(_, NoteDuration),
-	Beats is NoteDuration / Duration.
+    number(Duration), Duration > 0,
+    timeSignature(_, NoteDuration),
+    Beats is NoteDuration / Duration.
 durationToBeats([], 0).
 durationToBeats([Duration | Rest], Beats) :-
-	durationToBeats(Duration, Beat1),
-	durationToBeats(Rest, BeatsR),
-	Beats is Beat1 + BeatsR.
+    durationToBeats(Duration, Beat1),
+    durationToBeats(Rest, BeatsR),
+    Beats is Beat1 + BeatsR.
 
 Dur0.fromBeats(Beats) := Dur :-
-	Beats < 0 ->
-		Beats2 is -Beats,
-		beatsToDuration(Beats2, DurLen),
-		Dur = Dur0.put(len, DurLen).put(negative, true);
-	beatsToDuration(Beats, DurLen),
-	Dur = Dur0.put(len, DurLen).
+    Beats < 0 ->
+        Beats2 is -Beats,
+        beatsToDuration(Beats2, DurLen),
+        Dur = Dur0.put(len, DurLen).put(negative, true);
+    beatsToDuration(Beats, DurLen),
+    Dur = Dur0.put(len, DurLen).
 
 beatsToDuration(0, 0) :- !.
 beatsToDuration(Beats, Duration) :-
-	number(Beats), Beats > 0,
-	timeSignature(_, NoteDuration),
-	FloatDuration is NoteDuration / Beats,
-	normalizedDuration(FloatDuration, Duration).
+    number(Beats), Beats > 0,
+    timeSignature(_, NoteDuration),
+    FloatDuration is NoteDuration / Beats,
+    normalizedDuration(FloatDuration, Duration).
 
 Dur.norm() := Normalized :-
-	Dur.len = [_ | _] ->
-		Normalized = Dur.len;
-	normalizedDuration(Dur.len, Normalized).
+    Dur.len = [_ | _] ->
+        Normalized = Dur.len;
+    normalizedDuration(Dur.len, Normalized).
 
 normalizedDuration(Duration, Normalized) :-
-	(Duration > 128; Duration < 10 * epsilon) -> Normalized = [];
+    (Duration > 128; Duration < 10 * epsilon) -> Normalized = [];
 
-	NormDur is max(2 ** ceiling(log(Duration) / log(2)), 1),
-	(NormDur =:= Duration -> Normalized = [NormDur];
-		DurRest is 1 / (1 / Duration - 1 / NormDur),
-		normalizedDuration(DurRest, NormRest),
-		Normalized = [NormDur | NormRest]).
+    NormDur is max(2 ** ceiling(log(Duration) / log(2)), 1),
+    (NormDur =:= Duration -> Normalized = [NormDur];
+        DurRest is 1 / (1 / Duration - 1 / NormDur),
+        normalizedDuration(DurRest, NormRest),
+        Normalized = [NormDur | NormRest]).
 
 sumDurationsInv(Durs, Sum) :-
-	map_list(inverse, Durs, Invs),
-	sum_list(Invs, Sum).
+    map_list(inverse, Durs, Invs),
+    sum_list(Invs, Sum).
 
 dursInvCmp(Delta, Durs1, Durs2) :-
-	sumDurationsInv(Durs1, Sum1),
-	sumDurationsInv(Durs2, Sum2),
-	compare(Delta, Sum2, Sum1).
+    sumDurationsInv(Durs1, Sum1),
+    sumDurationsInv(Durs2, Sum2),
+    compare(Delta, Sum2, Sum1).
 
+Dur1.cmp(Dur2) := Delta :-
+    dursInvCmp(Delta, Dur1.len, Dur2.len).
--- muspl/duration.plt
+++ muspl/duration.plt
@@ -3,45 +3,44 @@
 :- include('testSong.plt').
 
 test(beats, [setup(ts34), cleanup(clear)]) :-
-	duration{len:[]}.beats() == 0,
-	duration{len:8}.beats() == 0.5,
-	duration{len:[1, 2]}.beats() == 6.
+    duration{len:[]}.beats() == 0,
+    duration{len:8}.beats() == 0.5,
+    duration{len:[1, 2]}.beats() == 6.
 test(beats2, [setup(ts68), cleanup(clear)]) :-
-	duration{len:[]}.beats() == 0,
-	duration{len:8}.beats() == 1,
-	duration{len:[1, 2]}.beats() == 12.
+    duration{len:[]}.beats() == 0,
+    duration{len:8}.beats() == 1,
+    duration{len:[1, 2]}.beats() == 12.
 
 test(fromBeats1, [setup(ts34), cleanup(clear)]) :-
-	duration{}.fromBeats(2).len == [2],
-	duration{}.fromBeats(4).len == [1],
-	duration{}.fromBeats(2.5).len == [2, 8].
+    duration{}.fromBeats(2).len == [2],
+    duration{}.fromBeats(4).len == [1],
+    duration{}.fromBeats(2.5).len == [2, 8].
 test(fromBeats2, [setup(ts68), cleanup(clear)]) :-
-	duration{}.fromBeats(2).len == [4],
-	duration{}.fromBeats(1).len == [8].
+    duration{}.fromBeats(2).len == [4],
+    duration{}.fromBeats(1).len == [8].
 
 test(negBeats, [setup(ts34), cleanup(clear)]) :-
-	duration{}.fromBeats(-2.5) = NegDur,
-	NegDur.len == [2, 8],
-	NegDur.get(negative),
-	NegDur.beats() == -2.5.
+    duration{}.fromBeats(-2.5) = NegDur,
+    NegDur.len == [2, 8],
+    NegDur.get(negative),
+    NegDur.beats() == -2.5.
 
 test(norm) :-
-	duration{len:1.6}.norm() == [2, 8],
-	duration{len:4}.norm() == [4],
-	duration{len:0}.norm() == [],
-	duration{len:999}.norm() == [],
-	duration{len:0.5}.norm() == [1, 1].
+    duration{len:1.6}.norm() == [2, 8],
+    duration{len:4}.norm() == [4],
+    duration{len:0}.norm() == [],
+    duration{len:999}.norm() == [],
+    duration{len:0.5}.norm() == [1, 1].
 
 test(add) :-
-	duration{len:8}.add(duration{len:[8]}).len == [4],
-	duration{len:8}.add(duration{len:4}).len == [4, 8],
-	duration{len:1}.add(duration{len:1}).len == [1, 1],
-	duration{len:[8, 2]}.add(duration{len:8}).len == [2, 4].
+    duration{len:8}.add(duration{len:[8]}).len == [4],
+    duration{len:8}.add(duration{len:4}).len == [4, 8],
+    duration{len:1}.add(duration{len:1}).len == [1, 1],
+    duration{len:[8, 2]}.add(duration{len:8}).len == [2, 4].
 
-test(dursInvCmp) :-
-	dursInvCmp(Delta1, [4, 8], 2), Delta1 == '>',
-	dursInvCmp(Delta2, [4, 8], [8, 4]), Delta2 == '=',
-	dursInvCmp(Delta3, [2, 2, 2], 1), Delta3 == '<'.
+test(cmp) :-
+    duration{len:[4, 8]}.cmp(duration{len:2}) == '>',
+    duration{len:[4, 8]}.cmp(duration{len:[8, 4]}) == '=',
+    duration{len:[2, 2, 2]}.cmp(duration{len:1}) == '<'.
 
 :- end_tests(duration).
-
